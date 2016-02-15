9ec8bb3f20655092d100f8b11440c6de8271e5b8
Manoj Jasti <manoj.jasti@gmail.com>
2016-02-14 21:51:01 -0600

    Test Commit

---
+++ Content/main.js
@@ -0,0 +1,9 @@
+﻿//$(".modal-wide").on("show.bs.modal", function () {
+//    var height = $(window).height() - 200;
+//    $(this).find(".modal-body").css("max-height", height);
+//});
+
+$(document).ready(function () {
+    $("#navbar-frame").load("../Views/Navbar.html");
+    $("#footer-frame").load("../Views/footer.html");
+});
